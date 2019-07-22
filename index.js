d0cba4ae57c7bf2263e440a926c1764bdecc9528
Mary Major <mary.major@example.com>
2019-07-22 09:01:26 +0000

    add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 exports.handler = async (word, event, context) => {
- word="India";
+ word="Japan";
  const sent="hello "+word;
  console.log(sent);
  return { statusCode: 200,body: sent };
